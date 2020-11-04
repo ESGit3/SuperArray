cec56a08ddd43623629a9076fc02058f7557a238
ESGit3 <ethanshan706@gmail.com>
2020-11-04 00:17:46 -0500

    add

--- Driver.java
+++ Driver.java
@@ -1,6 +1,18 @@
 public class Driver {
     public static void main(String[] args) {
         SuperArray a = new SuperArray();
+        a.add("a");
+        a.add("a");
+        a.add("a");
+        a.add("a");
+        a.add("a");
+        a.add("a");
+        a.add("a");
+        a.add("a");
+        a.add("a");
+        a.add("a");
+
+
         System.out.println(a.size());
     }
 }
